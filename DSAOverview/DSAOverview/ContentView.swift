7604a1b0882a6fda3f910f9dbab498c24daad255
AbhilashPalem258 <abhilashkumar258@gmail.com>
2024-10-04 08:46:24 -0400

    initial commit

---
+++ DSAOverview/DSAOverview/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DSAOverview
+//
+//  Created by Abhilash Palem on 04/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ DSAOverview/DSAOverview/DSAOverviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSAOverviewApp.swift
+//  DSAOverview
+//
+//  Created by Abhilash Palem on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DSAOverviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
